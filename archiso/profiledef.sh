27f4e284d3430fe6b29d58e82f1c9274acff9a94
Kaida-27 <157682708+Kaida-27@users.noreply.github.com>
2024-01-27 22:58:43 -0500

    Update profiledef.sh

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -24,6 +24,7 @@ file_permissions=(
   ["/usr/local/bin/livecd-sound"]="0:0:755"
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
-  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
+  ["/usr/local/bin/displaymanager-check"]="0:0:755"
+  ["/usr/local/bin/fixbtrfs"]="0:0:755"
   ["/usr/local/bin/fix-keys"]="0:0:755"
 )
